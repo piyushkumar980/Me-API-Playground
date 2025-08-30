4ea672094b60a4e3e2484805d779919e3c0fef8b
Piyush Kumar <piyushkumarofficial980@gmail.com>
2025-08-31 01:58:40 +0530

    final commit

--- server/src/config.js
+++ server/src/config.js
@@ -4,5 +4,5 @@ module.exports = {
   port: process.env.PORT || 5000,
   mongoURI: process.env.MONGO_URI,
   apiKey: process.env.API_KEY,
-  frontendURL: process.env.FRONTEND_URL || 'http://localhost:5173'
+  frontendURL: process.env.FRONTEND_URL || 'https://me-api-playground-mui0.onrender.com'
 };
